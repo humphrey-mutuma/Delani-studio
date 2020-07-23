436f4a60684700959185fd229d23e702c7787e4c
humphrey-mutuma <humphreymutuma01@gmail.com>
2020-07-23 20:55:30 +0300

    Add toggle effects to the what we do page.

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('.initiallyShown1').click(function(){
+        $('.initiallyHiden1').toggle();
+        $('.initiallyShown1').toggle();
+    });
+    $('.initiallyHiden1').click(function(){
+        $('.initiallyShown1').toggle();
+        $('.initiallyHiden1').toggle();
+    });
+    $('.initiallyShown2').click(function(){
+        $('.initiallyHiden2').toggle();
+        $('.initiallyShown2').toggle();
+    });
+    $('.initiallyHiden2').click(function(){
+        $('.initiallyShown2').toggle();
+        $('.initiallyHiden2').toggle();
+    });
+    $('.initiallyShown3').click(function(){
+        $('.initiallyHiden3').toggle();
+        $('.initiallyShown3').toggle();
+    });
+    $('.initiallyHiden3').click(function(){
+        $('.initiallyShown3').toggle();
+        $('.initiallyHiden3').toggle();
+    });
+
+});
+
